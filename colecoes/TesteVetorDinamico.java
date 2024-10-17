9d991c0f943bd29824867460e32a65c67893ae96
Isabel Santos <isabel.marques@fatec.sp.gov.br>
2024-10-17 18:03:45 -0300

    feat(colecoes): encerra o vetor de ints

--- colecoes/TesteVetorDinamico.java
+++ colecoes/TesteVetorDinamico.java
@@ -3,13 +3,20 @@ public class TesteVetorDinamico {
     public static void main(String[] args) throws Exception {
         //construir um vetor dinamico do tamanho 8
         //use o operador de inferencia de tipo
-        var teste = new VetorDinamico(8);
+        var teste = new VetorDinamico(4);
         var gerador = new Random();
         //loop infinito com while
         while (true){
             var valor =  gerador.nextInt(10) + 1;
-            //adicionar
-            teste.adicionar(valor);
+            var oQueFazer = gerador.nextDouble();
+            if(oQueFazer < 0.4){
+                System.out.println("Removendo...");
+                teste.remover();
+            }else{
+                //adicionar
+                System.out.println("Adicionando...");
+                teste.adicionar(valor);
+            }
             System.out.println(teste);
             System.out.println("======================");
             //dormir por 5 segundos
--- colecoes/VetorDinamico.java
+++ colecoes/VetorDinamico.java
@@ -2,10 +2,68 @@ public class VetorDinamico {
     private int qtde;
     private int cap;
     private int [] elementos;
+    private static final int CAPACIDADE_MINIMA = 4;
 
     public void adicionar(int e){
-        elementos[qtde] = e;
-        qtde++;
+        if (estaCheio()){
+            //aumentarCapacidade();
+            redimensionar(2);
+        }
+        elementos[qtde++] = e;
+    
+    }
+
+    private void redimensionar(double fator){
+        int[] aux;
+        aux = new int[(int)(cap * fator)];
+        for(int i = 0; i < qtde; i++){
+            aux[i] = elementos[i];
+        }
+        cap = (int)(cap * fator);
+        elementos = aux;
+    }
+    private void aumentarCapacidade(){
+        int[] aux;
+        aux = new int[cap * 2];
+        for(int i = 0; i < qtde; i++){
+            aux[i] = elementos[i];
+        }
+        cap = cap * 2;
+        elementos = aux;
+    }
+
+    private void reduzirCapacidadade(){
+        int[] aux;
+        aux = new int[cap / 2];
+        for(int i = 0; i < qtde; i++){
+            aux[i] = elementos[i];
+        }
+        cap = cap / 2;
+        elementos = aux;
+    }
+
+    public void remover(){
+        if(!estaVazio()){
+            qtde--;
+        }
+        if(estaUmQuartoCheio() && cap > CAPACIDADE_MINIMA){
+            redimensionar(0.5);
+        }
+    }
+    public boolean estaVazio(){
+        return qtde == 0;
+    }
+
+    public boolean estaUmQuartoCheio(){
+        return qtde <= cap / 4;
+    }
+    
+    public boolean estaCheio(){
+        return qtde == cap;
+        //return qtde == cap ? true : false;
+        //if(qtde == cap)
+           //return true;
+        //return false;
     }
 
     public VetorDinamico(int cap){
@@ -28,9 +86,5 @@ public class VetorDinamico {
         for(var i = 0; i < qtde; i++)
             sb.append(elementos[i]).append(" ");
         return sb.toString();
-    }
-
-    public static void main(String[] args) {
-        
     }
 }
