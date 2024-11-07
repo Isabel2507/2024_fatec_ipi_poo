9ea0855ffd1e52ba7417bc6e79daefc6d1b3027d
Isabel Santos <isabel.marques@fatec.sp.gov.br>
2024-11-07 16:09:30 -0300

    ap003(heranca): encerra apostila de heranca

---
+++ heranca/v4/Teste.java
@@ -0,0 +1,9 @@
+package heranca.v4;
+
+public class Teste { 
+    public static void main(String[] args) {
+        var p = new Pessoa("Ana", 20);
+        var a = new Aluno("Pedro", 19, 123456);
+    }
+    
+}
