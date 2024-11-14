0dc887e29698b878f4719eb8c94178de40122a91
Isabel Santos <isabel.marques@fatec.sp.gov.br>
2024-11-14 17:59:00 -0300

    ap09(bd): apagar e remover

---
+++ jdbc_pessoas/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        var p = new Pessoa();
+        p.setNome("João");
+        p.setFone("11223344");
+        p.setEmail("joao@email.com");
+        var dao = new PessoaDAO(); //data access object
+    }
+}
