3aaa0de3208c5bd20e85c741df14d2eeec05e4f2
Isabel Santos <isabel.marques@fatec.sp.gov.br>
2024-11-21 18:23:31 -0300

    poo(dao): adiciona apagar e mostrar

--- jdbc_pessoas/src/Pessoa.java
+++ jdbc_pessoas/src/Pessoa.java
@@ -1,11 +1,50 @@
-import lombok.*;
-
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-public class Pessoa {
+public class Pessoa{
     private int codigo;
     private String nome, fone, email;
-    
+
+    public Pessoa(){
+
+    }
+
+    public Pessoa(int codigo){
+        this.codigo = codigo;
+    }
+
+    public Pessoa(int codigo, String nome, String fone, String email){
+        this.codigo = codigo;
+        this.email = email;
+        this.fone =fone;
+        this.nome = nome;
+    }
+    public int getCodigo() {
+        return codigo;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getFone() {
+        return fone;
+    }
+    public String getNome() {
+        return nome;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setFone(String fone) {
+        this.fone = fone;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("nome: %s, fone: %s, email: %s", nome, fone, email);
+    }
+
 }
